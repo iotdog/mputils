4379f5160edbf379d3a7af5acf83519575c78f10
River Wang <jiang.w@vip.163.com>
2018-06-22 10:46:51 +0800

    add msgbox utils

---
+++ msgbox.js
@@ -0,0 +1,34 @@
+/**
+ * 显示简单提示框，包括确认按钮和消息正文
+ */
+const showAlertSimple = (message) => {
+  wx.showModal({
+    content: message,
+    showCancel: false
+  })
+}
+
+/**
+ * 显示标准提示框，包括确认和取消按钮、消息标题、消息正文
+ */
+const showAlertStandard = (title, message, confirm, cancel) => {
+  wx.showModal({
+    title: title,
+    content: message,
+    success: (res) => {
+      if(res.confirm) {
+        confirm()
+      } else {
+        cancel()
+      }
+    },
+    fail: (res) => {
+      console.log('failed to show modal: ', res)
+    }
+  })
+}
+
+module.exports = {
+  showAlertSimple: showAlertSimple,
+  showAlertStandard: showAlertStandard
+}
